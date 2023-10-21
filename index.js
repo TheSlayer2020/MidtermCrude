38c4dd5aee136c5fc3e5122bb012da8d55a5e054
TheSlayer2020 <occ.crausus.joshua@gmail.com>
2023-10-21 17:46:33 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,214 @@
+/* Start for control coding */
+var registerForm = document.querySelector("#register-form");
+var allInput = registerForm.querySelectorAll("input");
+var addBtn = document.querySelector("#add-btn");
+var modal = document.querySelector(".modal");
+var closeBtn = document.querySelector(".close-icon");
+addBtn.onclick = function () {
+  modal.classList.add("active");
+};
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+  /*var i;
+    for(i=0;i<allInput.length;i++){
+        allInput[i].value = "";
+    }*/
+});
+
+/* start all global variable */
+
+var userData = [];
+var profile_pic = document.querySelector("#profile-pic");
+var uploadPic = document.querySelector("#upload-field");
+var idEl = document.getElementById("id");
+var nameEl = document.querySelector("#name");
+var l_nameEl = document.getElementById("l-name");
+var emailEl = document.querySelector("#email");
+var officeEl = document.querySelector("#office-code");
+var jobTitleEl = document.querySelector("#job-title");
+var registerBtn = document.querySelector("#register-btn");
+var updateBtn = document.querySelector("#update-btn");
+var registerForm = document.querySelector("#register-form");
+var imgUrl;
+
+/* end all global variable */
+
+/* start register coding */
+
+registerBtn.onclick = function (e) {
+  e.preventDefault();
+  regitrationData();
+  getDataFromLocal();
+  registerForm.reset("");
+  closeBtn.click();
+  alert("Page Auto Refreash.");  // Display an alert
+
+    // Define a function to refresh the page after a specified time
+    function autoRefresh(time) {
+        setTimeout(function() {
+            location.reload();
+        }, time);
+    }
+
+    // Call the autoRefresh function with a delay of 3000 milliseconds (3 seconds)
+    autoRefresh(500);  // Refresh the page after 3 seconds (you can adjust the time)
+  
+};
+
+if (localStorage.getItem("userData") != null) {
+  userData = JSON.parse(localStorage.getItem("userData"));
+}
+
+function regitrationData() {
+  userData.push({
+    id: idEl.value,
+    name: nameEl.value,
+    l_name: l_nameEl.value,
+    email: emailEl.value,
+    officeCode: officeEl.value,
+    jobTitle: jobTitleEl.value,
+    profilePic: imgUrl == undefined ? "User-1.png" : imgUrl
+  });
+  var userString = JSON.stringify(userData);
+  localStorage.setItem("userData", userString);
+  swal("Good job!", "Registration Succes!", "success");
+}
+
+/* start returning data on page from localstorage  */
+
+var tableData = document.querySelector("#table-data");
+const getDataFromLocal = () =>{
+  userData.forEach((data, index) => {
+    tableData.innerHTML += `
+        
+        <tr index='${index}'>
+            <td> ${index + 1} </td>
+            <td> <img src="${data.profilePic}" width="40"></td>
+            <td> ${data.id} </td>
+            <td> ${data.name} </td>
+            <td> ${data.l_name} </td>
+            <td> ${data.email} </td>
+            <td> ${data.officeCode} </td>
+            <td> ${data.jobTitle} </td>
+            <td>
+
+                <button class="edit-btn"><i class="fa fa-eye"></i></button>
+                <button class="del-btn" style="background-color: rgb(196, 55, 55);"><i class="fa fa-trash"></i></button>
+            </td>
+        <tr>
+        `;
+  });
+
+  /* start delete coding */
+
+  var i;
+  var allDelBtn = document.querySelectorAll(".del-btn");
+  for (i = 0; i < allDelBtn.length; i++) {
+    allDelBtn[i].onclick = function () {
+      var tr = this.parentElement.parentElement;
+      var id = tr.getAttribute("index");
+      swal({
+        title: "Are you sure?",
+        text:
+          "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+      }).then((willDelete) => {
+        if (willDelete) {
+          userData.splice(id, 1);
+          localStorage.setItem("userData", JSON.stringify(userData));
+          tr.remove();
+          swal("Poof! Your imaginary file has been deleted!", {
+            icon: "success"
+          });
+        } else {
+          swal("Your imaginary file is safe!");
+        }
+      });
+    };
+  }
+
+  /* start update coding */
+
+  var allEdit = document.querySelectorAll(".edit-btn");
+  for (i = 0; i < allEdit.length; i++) {
+    allEdit[i].onclick = function () {
+      var tr = this.parentElement.parentElement;
+      var td = tr.getElementsByTagName("TD");
+      var index = tr.getAttribute("index");
+      var imgTag = td[1].getElementsByTagName("IMG");
+      var profilePic = imgTag[0].src;
+      var id = td[2].innerHTML;
+      var name = td[3].innerHTML;
+      var l_name = td[4].innerHTML;
+      var email = td[5].innerHTML;
+      var officeCode = td[6].innerHTML;
+      var jobTitle = td[7].innerHTML;
+      addBtn.click();
+      registerBtn.disabled = true;
+      updateBtn.disabled = false;
+      idEl.value = id;
+      nameEl.value = name;
+      l_nameEl.value = l_name;
+      emailEl.value = email;
+      officeEl.value = officeCode;
+      jobTitleEl.value = jobTitle;
+      profile_pic.src = profilePic;
+      updateBtn.onclick = function (e) {
+        userData[index] = {
+          id: idEl.value,
+          name: nameEl.value,
+          l_name: l_nameEl.value,
+          email: emailEl.value,
+          officeCode: officeEl.value,
+          jobTitle: jobTitleEl.value,
+          profilePic: uploadPic.value == "" ? profile_pic.src : imgUrl
+        };
+        localStorage.setItem("userData", JSON.stringify(userData));
+      };
+    };
+  }
+};
+
+getDataFromLocal();
+
+/* immage procesing */
+
+uploadPic.onchange = function () {
+  if (uploadPic.files[0].size < 1000000) {
+    var fReader = new FileReader();
+    fReader.onload = function (e) {
+      imgUrl = e.target.result;
+      profile_pic.src = imgUrl;
+      console.log(imgUrl);
+    };
+    fReader.readAsDataURL(uploadPic.files[0]);
+  } else {
+    alert(" File size is to long ");
+  }
+}
+
+
+// start search coding
+
+var searchEl = document.querySelectorAll("#empId");
+searchEl.oninput = function(){
+  searchFuc();
+  
+}
+
+function searchFuc(){
+    var tr = tableData.querySelectorAll("TR");
+    var filter = searchEl.value.toLowerCase();
+    var i;
+    for(i=0;i<tr.length;i++){
+      var id = tr[i].getElementsByTagName("TD")[2].innerHTML;
+      if(id.toLowerCase().indexOf(filter) > -1){
+          tr[i].style.display = "";
+      }else{
+          tr[i].style.display = "none";
+      }
+  }
+}
+
